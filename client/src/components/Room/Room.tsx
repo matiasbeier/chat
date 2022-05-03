55bb8ca4fe31ce78655deb90d4c23cd8bd812e9e
matiasbeier <matiasbeier1@gmail.com>
2022-05-02 22:36:08 -0300

    working in css

--- client/src/components/Room/Room.tsx
+++ client/src/components/Room/Room.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import useSocket from '../UseSocket';
 import s from './Room.module.css'
+import { BiSend } from "react-icons/bi";
 
 interface Mess {
     text?: string,
@@ -14,54 +15,56 @@ type ParamsId = {
 
 const Room = () => {
     const {idRoom}: ParamsId = useParams()
-  const {messages, sendMessage} = useSocket(idRoom)
+    const {messages, sendMessage} = useSocket(idRoom)
 
-  const [listMessages, setListMessages] = useState<Mess[]>([])
-  const [newMessage, setNewMessage] = useState<string>("");
+    const [listMessages, setListMessages] = useState<Mess[]>([])
+    const [newMessage, setNewMessage] = useState<string>("");
 
-  useEffect(() =>{
-      messages.text &&
-  setListMessages([...listMessages, messages])
-  }, [messages])
+    useEffect(() =>{
+        messages.text &&
+    setListMessages([...listMessages, messages])
+    }, [messages])
 
-  function handleMessage(e: ChangeEvent<HTMLInputElement>){
-      setNewMessage(e.target.value)
-  }
+    function handleMessage(e: ChangeEvent<HTMLInputElement>){
+        setNewMessage(e.target.value)
+    }
 
-  function handleSendMessage(e: React.FormEvent){
-      e.preventDefault()
-      sendMessage(newMessage);
-      setNewMessage("");
-  }
-  return (
-    <div className={s.containerChat}>
-        <div className={s.chat} /* disabled={true} */ > 
-          {
-              listMessages?.map((message) => {
-                  return (
-                      message.writtenByCurrentUser
-                          ? <p key={`${Math.random()}${message.id}${message?.text}`} 
-                              style={{textAlign: "justify"}} 
-                            >
-                                {`Yo: ${message?.text}`}
-                            </p>
-                          : <p key={`${Math.random()}${message.id}${message?.text}`} 
-                              style={{textAlign: "justify", wordBreak: "break-word"}} 
-                            >
-                                {`${message?.id}: ${message?.text}`}
-                            </p> 
-                      )
-                  })
-          }
+    function handleSendMessage(e: React.FormEvent){
+        e.preventDefault()
+        sendMessage(newMessage);
+        setNewMessage("");
+    }
+    return (
+        <div className={s.container}>
+            <div className={s.containerChat}>
+                <div className={s.chat} > 
+                {
+                    listMessages?.map((message) => {
+                        return (
+                            message.writtenByCurrentUser
+                                ? <p className={s.messageMe} key={`${Math.random()}${message.id}${message?.text}`} 
+                                    style={{textAlign: "justify", wordBreak: "break-word"}} 
+                                    >
+                                        {`Me: ${message?.text}`}
+                                    </p>
+                                : <p className={s.message} key={`${Math.random()}${message.id}${message?.text}`} 
+                                    style={{textAlign: "justify", wordBreak: "break-word"}} 
+                                    >
+                                        {`User${message?.id?.substring(0, 5)}: ${message?.text}`}
+                                    </p> 
+                            )
+                        })
+                }
+                </div>
+                <div className={s.containerSendMessages}>
+                    <form onSubmit={e => handleSendMessage(e)} className={s.form} >
+                        <input type="text" placeholder="message..." value={newMessage} onChange={(e)=>handleMessage(e)} className={s.input} />
+                        <button onClick={e => handleSendMessage(e)} className={s.button}><BiSend/></button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className={s.containerSendMessages}>
-            <form onSubmit={e => handleSendMessage(e)}>
-                <input type="text" placeholder="Escribe un mensaje..." value={newMessage} onChange={(e)=>handleMessage(e)}/>
-                <button onClick={e => handleSendMessage(e)} >Enviar</button>
-            </form>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Room
